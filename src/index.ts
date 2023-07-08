cd824b6a911e4890304e508f8f18d4f4b431bf74
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-07-08 16:36:07 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,29 @@
 
 // sayHello("World");
 
-async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  return await res.json();
+// async function getData() {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//   return await res.json();
+// }
+
+// getData().then((data) => console.log(data));
+
+//If you don't have ts-node, you need to use `npm install -D @types/node`
+// import fs from "fs";
+
+// const dir = fs.readdirSync(__dirname);
+// console.log(dir);
+
+import { type User } from "./type";
+
+async function getUserData() {
+  const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  const usersData: User[] = await users.json();
+  return usersData;
 }
 
-getData().then((data) => console.log(data));
+getUserData().then((data) => {
+  data.forEach((user) => {
+    console.log(user.address.geo.lat);
+  });
+});
